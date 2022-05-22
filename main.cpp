9214d1d7b2c7adef8091735381eccd27ccd801e5
Jannik Kreucher <jannik.kreucher@th-bingen.de>
2022-05-22 21:35:06 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* PROJECT:  MPRO Laboraufgabe 2
+ *
+ * DEVELOPERS: Jannik Kreucher
+ *             Nick Diendorf
+ *             Benedikt Wendling
+ */
+#include "mbed.h"
+
+
+int main() {
+
+    while(1) {
+
+    }
+}
